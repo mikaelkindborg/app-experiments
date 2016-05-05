8e5998f5ecf1fb50044b92d158d05603cc1890c8
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-05-05 13:11:11 +0200

    Experimental app

---
+++ SafeLife/app.js
@@ -0,0 +1,64 @@
+;(function(){
+
+var mEvents = []
+
+// Timer for att kolla events
+
+// Lista events
+
+app.getEvents = function()
+{
+	return mEvents
+}
+
+app.startMonitorEvents = function()
+{
+	setInterval(function() {}, 60000)
+}
+
+
+var addEvent = function(event)
+{
+	mEvents.push(event)
+}
+
+
+var checkEvent = function(event)
+{
+	// If event triggers, show alarm
+}
+
+// Location
+
+
+// Time
+
+
+// Event
+
+
+
+// Rule
+
+
+
+// Events
+
+addEvent(
+	{
+	name: 'Vakna', 
+	time: '9:00', 
+	place: 'Hemma'
+	})
+
+Vakna
+Frukost
+Gå hemmifrån
+Lunch
+Gå hem
+Middag
+Borsta tänderna
+Sova
+
+
+})();
