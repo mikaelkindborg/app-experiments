d2d4fb060dd42eaba378ec42828e495c4403e8ba
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-05-30 19:22:07 +0200

    Updated NU-appen.

--- Nuappen/app.js
+++ Nuappen/app.js
@@ -1,11 +1,13 @@
 ;(function(){
 
+window.app = {}
+
 var mEvents = []
 
 // Timer for att kolla events
 
 // Lista events
-
+ 
 app.getEvents = function()
 {
 	return mEvents
@@ -21,6 +23,7 @@ var addEvent = function(event)
 {
 	mEvents.push(event)
 }
+
 
 
 var checkEvent = function(event)
@@ -50,15 +53,101 @@ addEvent(
 	time: '9:00', 
 	place: 'Hemma'
 	})
+	
+	
+// Build NU list.
 
-Vakna
-Frukost
-Gå hemmifrån
-Lunch
-Gå hem
-Middag
-Borsta tänderna
-Sova
+function buildTodayList()
+{
+  var html = ''
+  html += addTodayListItem('Frukost', 'icon-eat.png')
+  html += addTodayListItem('Ta med gympapåsen', 'icon-reminder.png')
+  html += addTodayListItem('Ta med matlåda', 'icon-reminder.png')
+  html += addTodayListItem('Lunch', 'icon-eat.png')
+  addTodayList(html)
+}
 
+function addTodayListItem(text, icon)
+{
+  var html = 
+    '<li class="mdl-list__item">' +
+      '<img class="evo-list-icon-1" src="icons/' + icon + '" />' +
+      '<img class="evo-list-icon-2" src="icons/icon-start.png" />' +
+      '<img class="evo-list-icon-3" src="icons/icon-arrow-down.png" />' +
+      '<span class="mdl-list__item-primary-content">' +
+        text +
+      '</span>' +
+    '</li>'
+  return html
+}
+
+function addTodayList(html)
+{
+  $('.evo-list-today').append(html)
+  componentHandler.upgradeElement($('.evo-list-today').get(0))
+}
+
+function getDateString()
+{
+    var date = new Date()
+    var month = monthString(date.getMonth())
+    var day = date.getDate()
+    var weekday = weekdayString(date.getDay())
+    var hour = padNumber(date.getHours())
+    var minute = padNumber(date.getMinutes())
+    return weekday + ' ' + day + ' ' + month + ' ' + hour + ':' + minute
+}
+
+function padNumber(n)
+{
+  if (n < 10)
+  {
+    return '0' + n
+  }
+  else
+  {
+    return '' + n
+  }
+}
+
+function weekdayString(day)
+{
+  var days = [
+    'Måndag',
+    'Tisdag',
+    'Onsdag',
+    'Torsdag',
+    'Fredag',
+    'Lördag',
+    'Söndag']
+  return days[day]
+}
+
+function monthString(month)
+{
+  var months = [
+    'januari',
+    'februari',
+    'mars',
+    'april',
+    'maj',
+    'juni',
+    'juli',
+    'augusti',
+    'september',
+    'oktober',
+    'november',
+    'december']
+  return months[month]
+}
+
+buildTodayList()
+
+setInterval(
+  function() {
+    $('.evo-header-date').html(getDateString())
+  }, 
+  30000)
+$('.evo-header-date').html(getDateString())
 
 })();
